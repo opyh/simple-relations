a4c4c3bdf3683ebf25d6445726ba0a38a9d46409
Sebastian Felix Zappe <hypo@me.com>
2018-12-23 22:10:59 +0100

    Simplify FlowType definitions for `Relation`s

--- src/Relations.js
+++ src/Relations.js
@@ -52,18 +52,7 @@ export type RelationDescription<T, ThroughT> = {
 
 
 export type BelongsToRelationDescription<T, ThroughT> = {
-  collection: () => MongoCompatibleCollection<T>,
-  selector?: () => {},
-  options?: () => {},
-  humanName?: string,
-  humanNameSingular?: string,
-  humanNamePlural?: string,
-  humanCollectionName?: () => string,
-  humanCollectionNameSingular?: () => string,
-  helpText?: () => string,
-  through?: () => MongoCompatibleCollection<ThroughT>,
-  foreignKey?: () => string,
-  throughForeignKey?: () => string,
+  ...RelationDescription<T, ThroughT>,
   optional?: () => boolean,
   placeholder?: () => string,
   index?: number | string | {},
@@ -71,19 +60,7 @@ export type BelongsToRelationDescription<T, ThroughT> = {
 
 
 export type BelongsToRelation<T, ThroughT> = {
-  collection: () => MongoCompatibleCollection<T>,
-  selector: () => {},
-  options: () => {},
-  name: string,
-  humanName: string,
-  humanNameSingular: string,
-  humanNamePlural: string,
-  humanCollectionName: () => string,
-  humanCollectionNameSingular: () => string,
-  helpText: () => string,
-  through?: () => MongoCompatibleCollection<ThroughT>,
-  foreignKey: () => string,
-  throughForeignKey: () => string,
+  ...Relation<T, ThroughT>,
   optional: () => boolean,
   findOneUnbound: (options?: {}) => ?T,
   findOne: (options?: {}) => ?T,
@@ -93,36 +70,13 @@ export type BelongsToRelation<T, ThroughT> = {
 
 
 export type HasManyRelationDescription<T, ThroughT> = {
-  collection: () => MongoCompatibleCollection<T>,
-  selector?: () => {},
-  options?: () => {},
-  through?: () => MongoCompatibleCollection<ThroughT>,
-  foreignKey?: () => string,
-  throughForeignKey?: () => string,
-  humanName?: string,
-  humanNameSingular?: string,
-  humanNamePlural?: string,
-  humanCollectionName?: () => string,
-  humanCollectionNameSingular?: () => string,
-  helpText?: () => string,
+  ...RelationDescription<T, ThroughT>,
   nullifyForeignRelations?: () => boolean,
 };
 
 
 export type HasManyRelation<T, ThroughT> = {
-  collection: () => MongoCompatibleCollection<T>,
-  selector: () => {},
-  options: () => {},
-  through?: () => MongoCompatibleCollection<ThroughT>,
-  foreignKey?: () => string,
-  throughForeignKey: () => string,
-  name: string,
-  humanName: string,
-  humanNameSingular: string,
-  humanNamePlural: string,
-  humanCollectionName: () => string,
-  humanCollectionNameSingular: () => string,
-  helpText: () => string,
+  ...Relation<T, ThroughT>,
   findUnbound: (options?: {}) => MongoCompatibleCursor<T>,
   find: (options?: {}) => MongoCompatibleCursor<T>,
   nullifyForeignRelations: () => boolean,
